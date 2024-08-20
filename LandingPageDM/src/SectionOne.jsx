e0a0d0d2cbf0a4825855f51f4d1053e15a28ba9a
Rafael <132081624+LucasJoseArantes@users.noreply.github.com>
2024-08-20 10:43:43 -0300

    UPDATE: Unify class image-effect
    Use image-effect class for all position relative div with background-image and hover effect

--- LandingPageDM/src/SectionOne.jsx
+++ LandingPageDM/src/SectionOne.jsx
@@ -4,9 +4,9 @@ import Button from './Button';
 const SectionOne = () => {
     return (
         <div className='MainTitle'>
-            <div className='lamp-image'></div>
+            <div className='lamp-image image-effect' ></div>
             <h1>Transforme suas <b>ideias</b> em realidade com a <b>nossa voz</b></h1>
-            <div className='sound-image'></div>
+            <div className='sound-image image-effect'></div>
             <Button text='Conheça Nosso Trabalho' />
             <p>Locução profissional para eventos, rádio, televisão e muito mais...</p>
             
