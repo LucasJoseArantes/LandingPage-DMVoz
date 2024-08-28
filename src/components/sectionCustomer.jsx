fd54ccec9ce58dfe675347442361405e1b9adf6e
Rafael <lucas.js@estudante.iftm.edu.br>
2024-08-28 16:15:12 -0300

    Update costumer

--- src/components/sectionCustomer.jsx
+++ src/components/sectionCustomer.jsx
@@ -1,86 +1,55 @@
-import React from 'react';
-import TitleSubtitle from './titleSubtitle';
-import '../styles/sectionCustomer.css';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import Button from './button';
-import vasconcelosLogo from '../assets/images/sectionCostumer/vasconcelos.png';
-import culturaFmlogo from '../assets/images/sectionCostumer/cultura_fm.jpeg';
-import startLogo from '../assets/images/sectionCostumer/start.png';
-import unicredLogo from '../assets/images/sectionCostumer/unicred.png';
-import quotesLogo from '../assets/images/sectionCostumer/double-quotes.png';
-import { Navigation } from 'swiper/modules';
-
-const testimonials = [
-    {
-        name: 'Arroz Vasconcelos',
-        testimonial: 'Lorem ipsum dolor sit amet consectetur.Lorem ipsum dolor sit amet consectetur.Lorem ipsum dolor sit amet consectetur.',
-        logo: vasconcelosLogo,
-    },
-    {
-        name: 'Rádio Cultura FM',
-        testimonial: 'Lorem ipsum dolor sit amet consectetur.Lorem ipsum dolor sit amet consectetur.Lorem ipsum dolor sit amet consectetur.',
-        logo: culturaFmlogo,
-    },
-    {
-        name: 'Unicred',
-        testimonial: 'Lorem ipsum dolor sit amet consectetur.Lorem ipsum dolor sit amet consectetur.Lorem ipsum dolor sit amet consectetur.',
-        logo: startLogo,
-    },
-    {
-        name: 'Start Química',
-        testimonial: 'Lorem ipsum dolor sit amet consectetur.Lorem ipsum dolor sit amet consectetur.Lorem ipsum dolor sit amet consectetur.',
-        logo: unicredLogo,
-    },
-    {
-        name: 'Cliente restante 1',
-        testimonial: 'Lorem ipsum dolor sit amet consectetur.Lorem ipsum dolor sit amet consectetur.Lorem ipsum dolor sit amet consectetur.',
-        logo: unicredLogo,
-    },
-    {
-        name: 'Cliente restante 2',
-        testimonial: 'Lorem ipsum dolor sit amet consectetur.Lorem ipsum dolor sit amet consectetur.Lorem ipsum dolor sit amet consectetur.',
-        logo: unicredLogo,
-    },
-];
+import React from "react";
+import TitleSubtitle from "./titleSubtitle";
+import "../styles/sectionCustomer.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import Button from "./button";
+import quotesLogo from "../assets/images/sectionCostumer/double-quotes.png";
+import { Navigation } from "swiper/modules";
+import tesimonaljson from "../jsons/testimonials.json";
 
 const TestimonialCard = ({ name, testimonial, logo }) => {
-    return (
-        <div className="testimonial-card">
-            <img src={quotesLogo} alt="quotes" className="testimonial-quote" />
-            <p className="testimonial-text">"{testimonial}"</p>
-            <div className="testimonial-footer">
-                <img src={logo} alt={name} className="testimonial-logo" />
-                <h3 className="testimonial-name">{name}</h3>
-            </div>
-        </div>
-    );
+  return (
+    <div className="testimonial-card">
+      <img src={quotesLogo} alt="quotes" className="testimonial-quote" />
+      <p className="testimonial-text">"{testimonial}"</p>
+      <div className="testimonial-footer">
+        <img src={logo} alt={name} className="testimonial-logo" />
+        <h3 className="testimonial-name">{name}</h3>
+      </div>
+    </div>
+  );
 };
 
-
 const Customer = () => {
-    return (
-        <section className='sectionImage sectionCustomer'>
-            <TitleSubtitle title="Depoimentos" subtitle="Saiba quem são nossos parceiros, e o que têm a dizer sobre nós" />
-            <Swiper
-                className="mySwiper"
-                spaceBetween={30}
-                slidesPerView={4}
-                navigation={{
-                }}
-                modules={[Navigation]}
-            >
-                {testimonials.map((testimonial, index) => (
-                    <SwiperSlide key={index}>
-                        <TestimonialCard {...testimonial} />
-                    </SwiperSlide>
-                ))}
-            </Swiper>
-            <Button text="Estou Interessado" />
-        </section>
-    );
+  return (
+    <section className="sectionImage sectionCustomer">
+      <TitleSubtitle
+        title="Depoimentos"
+        subtitle="Saiba quem são nossos parceiros, e o que têm a dizer sobre nós"
+      />
+      <div className="swiperContainer">
+    
+        <Swiper
+          className="mySwiper"
+          spaceBetween={35}
+          slidesPerView={4}
+          navigation={{}}
+          modules={[Navigation]}
+          style={{padding: '0px 35px'}}
+        >
+          {tesimonaljson.map((testimonial, index) => (
+            <SwiperSlide key={index}>
+              <TestimonialCard {...testimonial} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+      <Button text="Estou Interessado" />
+    </section>
+  );
 };
 
 export default Customer;
