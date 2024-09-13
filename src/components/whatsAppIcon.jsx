5d4a4ad9a5b0c46ae48512ebf116f9c1424405d6
Rafael <lucas.js@estudante.iftm.edu.br>
2024-09-13 09:35:16 -0300

    Update whatsAppIcon.jsx

--- src/components/whatsAppIcon.jsx
+++ src/components/whatsAppIcon.jsx
@@ -5,7 +5,7 @@ import '../styles/app.css';
 export default function Whatsapp() {
   return (
     <FloatingWhatsApp
-      phoneNumber='+55349812-5957'
+      phoneNumber='+5534998125957'
       avatar='https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/HD_transparent_picture.png/800px-HD_transparent_picture.png?20200606142532'
       accountName='DMBot'
       chatMessage='Olá, como posso te ajudar?'
